ee464897ca81947ff02da33e43b40cc3ebebce5b
Chris Spring <chris@nolaspring.net>
2017-11-02 21:46:37 -0400

    yoed up an api

---
+++ configure.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+### This file is to help install resources to install dependancies for local termanal based development
+
+#[sudo]  npm install -g generator-express-rest-api
+yo express-rest-api
+npm run install-local
+
+#yo express-rest-api:controller
+
+#yo express-rest-api:service
+
+#yo express-rest-api:service
+
+#yo express-rest-api:repository
