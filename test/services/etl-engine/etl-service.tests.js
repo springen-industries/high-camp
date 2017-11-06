671f7772dbebb23e2931a6422da0a38e6b25a9fe
Chris Spring <chris@nolaspring.net>
2017-11-06 11:16:35 -0500

    test for function exists written and failing

---
+++ test/services/etl-engine/etl-service.tests.js
@@ -0,0 +1,78 @@
+describe('EtlService Tests', function() {
+
+ beforeEach(function(){
+ })
+  //
+  // describe('collectInput', function() {
+  //   it('should be a function', function(done) {
+  //     expect(etlService.collectInput).to.be.a('function');
+  //     done();
+  //   });
+  //
+  //   it('should iterate over signals using forEach', function(done) {
+  //     var mock = sinon.mock(signals).expects("forEach").atLeast(1);
+  //     etlService.collectInput(signals);
+  //     mock.verify();
+  //     done();
+  //   });
+  //
+  //   it('should return an array',function(done) {
+  //     var etlResult = etlService.collectInput(signals);
+  //     expect(etlResult).to.be.an('Array');
+  //     done();
+  //   });
+  //   it('should return the same number of objects that get passed', function(done){
+  //     var etlResult = etlService.collectInput(signals);
+  //     expect(etlResult.length).to.not.equal(0);
+  //     done();
+  //   });
+  //   it('should call sample for every element in signal array ', function(done){
+  //     etlSpy = sinon.spy(etlService,"sample");
+  //     var etlResult = etlService.collectInput(signals);
+  //     // console.log(etlSpy);
+  //     expect(etlSpy.callCount).to.equal(signals.length);
+  //     done();
+  //   });
+  //
+  // });
+
+
+
+
+
+
+
+  // describe('parseSamples', function() {
+  //   it('should be a function', function(done) {
+  //     expect(etlService.parseSamples).to.be.a('function');
+  //   });
+  // });
+  //
+  // describe('processSamples', function() {
+  //   it('should be a function', function(done) {
+  //     expect(etlService.processSamples).to.be.a('function');
+  //   });
+  // });
+  //
+  describe('beginStateLoop', function() {
+    it('should be a function', function(done) {
+      expect(etlService.beginStateLoop).to.be.a('function');
+    });
+  });
+  //
+  // describe('completeStateLoop', function() {
+  //   it('should be a function', function(done) {
+  //     expect(etlService.completeStateLoop).to.be.a('function');
+  //   });
+  // });
+  //
+  //
+  // describe('calculateStatefuls', function() {
+  //   it('should be a function', function(done) {
+  //     expect(etlService.calculateStatefuls).to.be.a('function');
+  //   });
+  // });
+
+
+
+});
