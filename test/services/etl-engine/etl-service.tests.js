7629175e6268bcf7dc063e582025b383331de7de
Chris Spring <chris@nolaspring.net>
2017-11-07 12:06:06 -0500

    corrected some syntax

--- test/services/etl-engine/etl-service.tests.js
+++ test/services/etl-engine/etl-service.tests.js
@@ -6,16 +6,25 @@ describe('EtlService Tests', function() {
 
    var signals = DataMockFactory.createSignals();
 
-  })
+ });
 
 
 
 
+  describe('collectInput', function() {
+    it('should be a function', function(done) {
+      expect(etlService.collectInput).to.be.a('function');
+      done();
+    });
+
+    it('should iterate over signals using forEach', function(done) {
+      var mock = sinon.mock(signals).expects("forEach").atLeast(1);
+      etlService.collectInput(signals);
+      mock.verify();
+      done();
+    });
 
 
-
-<<<<<<< HEAD
-=======
     /// synchronus tests
     it('should return an array',function(done) {
       var etlResult = etlService.collectInput(signals);
@@ -37,54 +46,18 @@ describe('EtlService Tests', function() {
     //   expect(etlSpy.callCount).to.equal(signals.length);
       done();
     });
->>>>>>> dev.etl.collectInput
 
-    describe('collectInput', function() {
-      it('should be a function', function(done) {
-        expect(etlService.collectInput).to.be.a('function');
-        done();
-      });
-
-      it('should iterate over signals using forEach', function(done) {
-        var mock = sinon.mock(signals).expects("forEach").atLeast(1);
-        etlService.collectInput(signals);
-        mock.verify();
-        done();
-      });
-
-
-      /// synchronus tests
-      it('should return an array',function(done) {
-        var etlResult = etlService.collectInput(signals);
-        expect(etlResult).to.be.an('Array');
-        done();
-      });
-      it('should return the same number of objects that get passed', function(done){
-        var etlResult = etlService.collectInput(signals);
-        expect(etlResult.length).to.not.equal(0);
-        done();
-      });
-      it('should call sample for every element in signal array ', function(done){
-        etlSpy = sinon.spy(etlService,"sample");
-        var etlResult = etlService.collectInput(signals);
-        // console.log(etlSpy);
-        expect(etlSpy.callCount).to.equal(signals.length);
-        done();
-      });
-
-    //asynchronus testss
-    describe('colectInput -- Response', function(){
-      it('should set up an asynch callbackSpy for when sample operation completes', function(done){
-         etlResult = etlService.collectInput(signals,callback);
-         expect(callback.called).to.be.true;
-         done();
-        });
+  //asynchronus testss
+  describe('colectInput -- Response', function(){
+    it('should set up an asynch callbackSpy for when sample operation completes', function(done){
+       etlResult = etlService.collectInput(signals,callback);
+       expect(callback.called).to.be.true;
+       done();
       });
     });
-
-describe('beginStateLoop', function() {
-    it('should be a function', function(done) {
-      expect(etlService.beginStateLoop).to.be.a('function');
+    it('callback should get passed an array with ', function(){
+      etlResult = etlService.collectInput(signals.callback);
+      expect(callback.called)
     });
   });
 
