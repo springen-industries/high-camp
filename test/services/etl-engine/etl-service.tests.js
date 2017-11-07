a0dec07b9cdc824a06daafbe40b0ec025e477e66
Chris Spring <chris@nolaspring.net>
2017-11-07 10:15:13 -0500

    treating as asynch function so all synch methods fail

--- test/services/etl-engine/etl-service.tests.js
+++ test/services/etl-engine/etl-service.tests.js
@@ -4,7 +4,7 @@ describe('EtlService Tests', function() {
 
  beforeEach(function(){
    var signals = DataMockFactory.createSignals();
- })
+  })
 
   describe('collectInput', function() {
     it('should be a function', function(done) {
@@ -14,33 +14,32 @@ describe('EtlService Tests', function() {
 
     it('should iterate over signals using forEach', function(done) {
       var mock = sinon.mock(signals).expects("forEach").atLeast(1);
-      etlService.collectInput(signals);
+      etlService.collectInput(signals,callbackSpy);
       mock.verify();
       done();
     });
 
     it('should return an array',function(done) {
-      var etlResult = etlService.collectInput(signals);
+      var etlResult = etlService.collectInput(signals,callbackSpy);
       expect(etlResult).to.be.an('Array');
       done();
     });
     it('should return the same number of objects that get passed', function(done){
-      var etlResult = etlService.collectInput(signals);
+      var etlResult = etlService.collectInput(signals,callbackSpy);
       expect(etlResult.length).to.not.equal(0);
       done();
     });
     it('should call sample for every element in signal array ', function(done){
       etlSpy = sinon.spy(etlService,"sample");
-      var etlResult = etlService.collectInput(signals);
+      var etlResult = etlService.collectInput(signals,callbackSpy);
       // console.log(etlSpy);
       expect(etlSpy.callCount).to.equal(signals.length);
       done();
     });
   describe('colectInput -- Response', function(){
-    it('should set up an asynch callback for when sample operation completes', function(done){
-       var callback = sinon.spy();
-       etlResult = etlService.collectInput(signals,callback);
-       expect(callback.called).to.be.true;
+    it('should set up an asynch callbackSpy for when sample operation completes', function(done){
+       etlResult = etlService.collectInput(signals,callbackSpy);
+       expect(callbackSpy.called).to.be.true;
        done();
       });
     });
