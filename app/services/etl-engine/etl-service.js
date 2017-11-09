745924072748167af255084f2d4101ac55f44f7e
Chris Spring <chris@nolaspring.net>
2017-11-09 10:23:40 -0500

    split logic such that if a callback is included as the last variable we get back a promise

--- app/services/etl-engine/etl-service.js
+++ app/services/etl-engine/etl-service.js
@@ -4,13 +4,31 @@ function EtlService() {
 }
 
 function collectInput(signals, callback) {
+  if (callback){
+    collectInputAsynch(signals,callback);
+  } else {
+    return collectInputSynch(signals);
+  }
+}
+
+
+function collectInputSynch(signals){
+  var ra = [];
+  signals.forEach(function sampleSignal(signal){
+    ra.push(sample(signal.target,signal.protocol));
+  });
+  return ra;
+}
+
+function collectInputAsynch(signals,callback){
   return new Promise(function(fulfill,reject){
      signals.forEach(function sampleSignal(signal){
-       sample(signal.target,signal.protocol);
+       sample(signal.target,signal.protocol,callback);
      });
    });
+ }
 
-}
+
 
 function sample(signal){
     return new Promise(function(fulfill, reject){
