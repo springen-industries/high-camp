38ef27b3eae9a60d0968450e14241d6b1885d2dd
Chris Spring <chris@nolaspring.net>
2017-11-13 14:15:18 -0500

    created schema object 'observation'

--- app/data.js
+++ app/data.js
@@ -1,11 +1,15 @@
 var mongoose = require('mongoose');
 global.SignalSchema = new mongoose.Schema({
      signal: {
+       identifier:  mongoose.Schema.Types.ObjectId,
        mode: String,
        target: String,
-       sampleTime: String,
-       lastReading:  Object}
-     }
-    //  schedule: {
-    //    type: Long, min: 2600 }
-   );
+       sampleTime: {type: Date, default: Date.now},
+       lastReading:  Object
+     },
+     observation: {
+       sampleTime:  Number,
+       signalIdentifier: mongoose.Schema.Types.ObjectId,
+       reading: Object
+      }
+     });
