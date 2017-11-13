96af97b5731cdd06ad54d9036e9b60b1582cb72a
Chris Spring <chris@nolaspring.net>
2017-11-13 13:59:13 -0500

    organized and succintified tests, started on .sample

--- test/services/etl-engine/etl-service.tests.js
+++ test/services/etl-engine/etl-service.tests.js
@@ -2,61 +2,70 @@ var DataMockFactory = require("test/DataMockFactory.js");
 
 describe('EtlService Tests', function() {
 
-  describe('collectInput', function() {
+  describe('function collectInput', function() {
+
     it('should be a function', function(done) {
       expect(etlService.collectInput).to.be.a('function');
       done();
     });
+    describe('synchronus tests', function(){
+      it('should iterate over signals using forEach', function(done) {
+        var mock = sinon.mock(signals).expects("forEach").atLeast(1);
+        etlService.collectInput(signals);
+        mock.verify();
+        done();
+      });
 
-    it('should iterate over signals using forEach', function(done) {
-      var mock = sinon.mock(signals).expects("forEach").atLeast(1);
-      etlService.collectInput(signals);
-      mock.verify();
-      done();
-    });
 
+      /// synchronus tests
+      it('should return an array',function(done) {
+        var etlResult = etlService.collectInput(signals);
+        expect(etlResult).to.be.an('Array');
+        done();
+      });
 
-    /// synchronus tests
-    it('should return an array',function(done) {
-      var etlResult = etlService.collectInput(signals);
-      expect(etlResult).to.be.an('Array');
-      done();
-    });
-    it('should return the same number of objects that get passed', function(done){
-      var etlResult = etlService.collectInput(signals);
-      expect(etlResult.length).to.not.equal(0);
-      done();
-    });
-
-    it('TODO: FIX SINON SPY -should call sample for every element in signal array ', function(done){
-    //   etlSpy = sinon.spy(etlService,"sample");
-    //   if (etlSpy.stackTrace.indexOf("Error") > 0) {
-    //     throw new new exception(etlSpy.stackTrace);
-    //   }
-    //   var etlResult = etlService.collectInput(signals);
-    //   expect(etlSpy.callCount).to.equal(signals.length);
-      done();
-    });
+      it('should return the same number of objects that get passed', function(done){
+        var etlResult = etlService.collectInput(signals);
+        expect(etlResult.length).to.not.equal(0);
+        done();
+      });
   });
+  describe("asynchronus tests", function(){
   //asynchronus testss
-  describe('colectInput --asynchronus execution and response', function(){
-    it('should return a promise when overloaded with a callback', function(done){
-       expect(etlService.collectInput(signals,callback)).to.be.a("Promise");
-       done();
+      it('should return a promise', function(done){
+         expect(etlService.collectInput(signals,callback)).to.be.a("Promise");
+         done();
+      });
+  });
+});
+// sample tests
+describe('function - sample', function(){
+  it('should exist in etlService', function(done){
+    expect(etlService.sample).to.be.a("function");
+    done();
+  });
+
+  describe("synchronus tests", function(){
+    it('should return an array', function(done){
+      expect(etlService.sample(signals)).to.be.an("Array");
+      done();
+    });
+    it('should be the length of array passed as argument',function (done){
+      expect(etlService.sample(signals).length).to.equal(signals.length);
+      done();
+    });
+    it('should contain Observation objects',function(done){
+      expect(etlService.sample(signals)[0]).to.be.an("Observation");
     });
   });
 
-  describe('sample', function(){
-    it('should exist in etlService', function(done){
-      expect(etlService.sample).to.be.a("function");
-      done();
-    });
+  describe("asynchronus tests",function(){
     it('should return a promise when called overloaded with a callback', function(done){
       var emptyCallback = function(){};
       expect(etlService.sample(signals,emptyCallback)).to.be.a("Promise");
       done();
     });
-
   });
+});
 
 });
